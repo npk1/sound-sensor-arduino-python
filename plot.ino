2d40675997a874da1eb16d01485105544ed6446b
Navaprakash <npk_ece@yahoo.in>
2019-08-02 16:22:48 +0530

    Add files via upload

---
+++ plot.ino
@@ -0,0 +1,17 @@
+
+\* created by npk1 *\
+
+
+const int analogIn = A0;
+int analogVal = 0;
+
+void setup() {
+  Serial.begin(19200);
+}
+
+void loop() {
+
+  analogVal = analogRead(analogIn);
+  Serial.println(analogVal);
+  delay(1000);
+}
